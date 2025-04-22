3db82fdac2522ce30272806ff38fbf0b864c415b
René Rafael <rene.rafael@groupsolver.com>
2025-04-22 11:59:22 +0200

    chore(nvim): remove `diffview.nvim` since it's not the preffered way to solve conflicts

--- roles/nvim/files/nvim/lua/plugins/neogit.lua
+++ roles/nvim/files/nvim/lua/plugins/neogit.lua
@@ -2,7 +2,6 @@ return {
   "NeogitOrg/neogit",
   dependencies = {
     "nvim-lua/plenary.nvim", -- required
-    "sindrets/diffview.nvim", -- optional
     "ibhagwan/fzf-lua", -- optional
   },
   config = true,
