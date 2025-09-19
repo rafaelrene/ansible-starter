a0b4ce18fa29ce47dbb20becd27e4c5f132e6caa
René Rafael <rene.rafael@groupsolver.com>
2025-09-19 10:49:00 +0200

    fix(nvim): Fix auto-fix on save for eslint

--- roles/nvim/files/nvim/lua/plugins/lsp.lua
+++ roles/nvim/files/nvim/lua/plugins/lsp.lua
@@ -76,7 +76,7 @@ return {
           vim.api.nvim_create_autocmd("BufWritePre", {
             callback = function(event)
               if util.get_active_client_by_name(event.buf, "eslint") then
-                vim.cmd("EslintFixAll")
+                vim.cmd("LspEslintFixAll")
               end
             end,
           })
